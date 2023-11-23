9f8b67620a6c2d24de99375a5dd600a104091320
Nigel Douglas <126002808+nigeldouglas-itcarlow@users.noreply.github.com>
2023-11-23 20:00:23 +0000

    Update TaskManager.s.sol

--- task-manager/TaskManager.s.sol
+++ task-manager/TaskManager.s.sol
@@ -1,35 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
-import "../src/TaskManager.sol";
+import { Script, console2 } from "forge-std/Script.sol";
+import "../src/Counter.sol";
 
-contract TaskManagerTest is Test {
-    TaskManager public taskManager;
-    address public alice;
-    address public bob;
+contract CounterScript is Script {
+    function setUp() public {}
 
-    function setUp() public {
-        taskManager = new TaskManager();
-        alice = makeAddr("Alice");
-        bob = makeAddr("Bob");
-    }
-
-    function run() public { // Implement the run function
-        testInitial();
-        testSingleTask();
-        testMultipleTasks();
-    }
-
-    function testInitial() public {
-        // TODO
-    }
-
-    function testSingleTask() public {
-        // TODO
-    }
-
-    function testMultipleTasks() public {
-        // TODO
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        
+        Counter ctr = new Counter();
+        
+        vm.stopBroadcast();
     }
 }
