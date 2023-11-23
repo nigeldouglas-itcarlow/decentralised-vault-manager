43c6250806382593e81beaab21ac552b567272b5
Nigel Douglas <126002808+nigeldouglas-itcarlow@users.noreply.github.com>
2023-11-23 20:01:20 +0000

    Update TaskManager.s.sol

--- task-manager/TaskManager.s.sol
+++ task-manager/TaskManager.s.sol
@@ -2,16 +2,16 @@
 pragma solidity ^0.8.13;
 
 import { Script, console2 } from "forge-std/Script.sol";
-import "../src/Counter.sol";
+import "../src/TaskManager.sol";
 
-contract CounterScript is Script {
+contract TaskManagerScript is Script {
     function setUp() public {}
 
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
         
-        Counter ctr = new Counter();
+        TaskManager ctr = new TaskManager();
         
         vm.stopBroadcast();
     }
