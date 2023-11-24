6d333f3e8b88bb4b525a4ba1669e8d58181cbc23
Nigel Douglas <126002808+nigeldouglas-itcarlow@users.noreply.github.com>
2023-11-24 17:25:47 +0000

    Update VaultManager.s.sol

--- vault-manager/VaultManager.s.sol
+++ vault-manager/VaultManager.s.sol
@@ -4,14 +4,14 @@ pragma solidity ^0.8.13;
 import { Script, console2 } from "forge-std/Script.sol";
 import "../src/VaultManager.sol";
 
-contract TaskManagerScript is Script {
+contract VaultManagerScript is Script {
     function setUp() public {}
 
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
         
-        TaskManager ctr = new TaskManager();
+        VaultManager ctr = new VaultManager();
         
         vm.stopBroadcast();
     }
