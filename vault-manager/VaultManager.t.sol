32f139465d6a1c5e678fb6873b4443a6d74d34f2
Nigel Douglas <126002808+nigeldouglas-itcarlow@users.noreply.github.com>
2023-11-24 13:28:15 +0000

    'assert' not 'Assert'

--- vault-manager/VaultManager.t.sol
+++ vault-manager/VaultManager.t.sol
@@ -6,11 +6,11 @@ import "./VaultManager.sol";
 import "ds-test/test.sol";
 
 // Test contract for VaultManager
-contract TestVaultManager {
+contract TestVaultManager is DSTest {
     VaultManager vaultManager;
 
     // Runs before each test function
-    function beforeEach() public {
+    function setUp() public {
         vaultManager = new VaultManager();
     }
 
@@ -18,9 +18,9 @@ contract TestVaultManager {
     function testAddVault() public {
         // Add a vault and perform assertions
         uint256 vaultId = vaultManager.addVault();
-        Assert.equal(vaultManager.getVaultsLength(), 1, "Vault count should be 1");
-        Assert.equal(vaultManager.getMyVaults().length, 1, "My vault count should be 1");
-        Assert.equal(vaultManager.getVault(vaultId).owner, address(this), "Owner should be the test contract");
+        assertEq(vaultManager.getVaultsLength(), 1, "Vault count should be 1");
+        assertEq(vaultManager.getMyVaults().length, 1, "My vault count should be 1");
+        assertEq(vaultManager.getVault(vaultId).owner, address(this), "Owner should be the test contract");
     }
 
     // Test case for deposit function
@@ -31,8 +31,8 @@ contract TestVaultManager {
 
         // Deposit into the vault and perform assertions
         vaultManager.deposit{ value: depositAmount }(vaultId);
-        Assert.equal(vaultManager.getVault(vaultId).balance, depositAmount, "Vault balance should be equal to deposit amount");
-        Assert.equal(address(this).balance, initialBalance - depositAmount, "Contract balance should be reduced by deposit amount");
+        assertEq(vaultManager.getVault(vaultId).balance, depositAmount, "Vault balance should be equal to deposit amount");
+        assertEq(address(this).balance, initialBalance - depositAmount, "Contract balance should be reduced by deposit amount");
     }
 
     // Test case for withdraw function
@@ -43,13 +43,13 @@ contract TestVaultManager {
 
         // Attempt to withdraw more than deposited (should fail)
         bool success = address(vaultManager).call{ value: depositAmount + 1 }(abi.encodeWithSelector(vaultManager.withdraw.selector, vaultId, depositAmount + 1));
-        Assert.isFalse(success, "Should fail to withdraw more than deposited");
+        assertFalse(success, "Should fail to withdraw more than deposited");
 
         // Withdraw valid amount and perform assertions
         uint256 initialBalance = address(this).balance;
         vaultManager.withdraw(vaultId, depositAmount);
-        Assert.equal(vaultManager.getVault(vaultId).balance, 0, "Vault balance should be 0 after withdrawal");
-        Assert.equal(address(this).balance, initialBalance, "Contract balance should be restored after withdrawal");
+        assertEq(vaultManager.getVault(vaultId).balance, 0, "Vault balance should be 0 after withdrawal");
+        assertEq(address(this).balance, initialBalance, "Contract balance should be restored after withdrawal");
     }
 
     // Additional tests for ownership constraints can be added here
@@ -58,8 +58,8 @@ contract TestVaultManager {
     function testEvents() public {
         uint256 vaultId = vaultManager.addVault();
         bool success = address(vaultManager).call{ value: 100 }(abi.encodeWithSelector(vaultManager.deposit.selector, vaultId));
-        Assert.isTrue(success, "Deposit should succeed");
+        assertTrue(success, "Deposit should succeed");
         success = address(vaultManager).call(abi.encodeWithSelector(vaultManager.withdraw.selector, vaultId, 50));
-        Assert.isTrue(success, "Withdraw should succeed");
+        assertTrue(success, "Withdraw should succeed");
     }
 }
