625cad63bd0b5ba9b38893581fe2c9e0f3a1a68d
Nigel Douglas <126002808+nigeldouglas-itcarlow@users.noreply.github.com>
2023-12-02 21:42:17 +0000

    Update VaultManager.s.sol

--- vault-manager/VaultManager.s.sol
+++ vault-manager/VaultManager.s.sol
@@ -1,15 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+
 import {Script, console2} from "forge-std/Script.sol";
 import "../src/VaultManager.sol";
 
 contract VaultManagerScript is Script {
     function setUp() public {}
-    
+
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
-        VaultManager vm = new VaultManager();
+
+        // Remove the unused variable if not needed
+        // VaultManager vm = new VaultManager();
+
         vm.stopBroadcast();
     }
 }
