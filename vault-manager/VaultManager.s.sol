a133cde7273fe4b97d69c879ac3454cf869bbdca
Nigel Douglas <126002808+nigeldouglas-itcarlow@users.noreply.github.com>
2023-12-03 19:55:19 +0000

    /script

--- vault-manager/VaultManager.s.sol
+++ vault-manager/VaultManager.s.sol
@@ -11,7 +11,16 @@ contract VaultManagerScript is Script {
         VaultManager vm = new VaultManager();
         console2.log("Contract Address:", address(vm));
         console2.log("cast call", address(vm), "--rpc-url local");
+
+        console2.log("cast call", address(vm), "\"addVault()\" --rpc-url local");
+        console2.log("cast call", address(vm), "\"getVault()\" --rpc-url local");
+
+        console2.log("cast call", address(vm), "\"deposit()\" --rpc-url local");
+        console2.log("cast call", address(vm), "\"withdraw()\" --rpc-url local");
+
+        console2.log("cast call", address(vm), "\"getVaultsLength()\" --rpc-url local");
         console2.log("cast call", address(vm), "\"getMyVaults()\" --rpc-url local");
+
         console2.log("cast balance", address(vm), "--rpc-url https://rpc.ankr.com/eth_sepolia | cast from-wei");
         console2.log("cast lookup-address", address(vm), "--rpc-url https://rpc.ankr.com/eth_goerli");
     }
