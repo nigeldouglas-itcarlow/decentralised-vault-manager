a8d781700e604120d488d5ca9f80935b722f423f
Nigel Douglas <126002808+nigeldouglas-itcarlow@users.noreply.github.com>
2023-12-03 00:03:08 +0000

    Update VaultManager.s.sol

--- vault-manager/VaultManager.s.sol
+++ vault-manager/VaultManager.s.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+
 import {Script, console2} from "forge-std/Script.sol";
 import "../src/VaultManager.sol";
 
@@ -9,5 +10,6 @@ contract VaultManagerScript is Script {
     function run() public {
         VaultManager vm = new VaultManager();
         console2.log("Contract Address:", address(vm));
+        console2.log("cast call", address(vm), "--rpc-url local");
     }
 }
